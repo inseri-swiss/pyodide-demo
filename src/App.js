a854a8dfce414da128b635c49827d75209dab253
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-03-25 19:59:50 +0100

    add initial pyodide code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import loadPyodide from 'loadPyodide'
+import { useState } from 'react';
+
+let pyodide = null
+
+async function initPyodide() {
+  pyodide = await loadPyodide({
+    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.1/full/",
+  });
+};
+
+function runPy(code, setOutput){
+  if(pyodide){
+    setOutput(pyodide.runPython(code))
+  } else {
+    setOutput("Not loaded")
+  }
+}
+initPyodide();
 
 function App() {
+  const [code, setCode] = useState("");
+  const [output, setOutput] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <textarea name="Text1" value={code} onChange={event => setCode(event.target.value)} cols="40" rows="5"/>
+        <br/>
+        <button onClick={()=>runPy(code, setOutput)}>Run Code</button>
+        <br/>
+        <div>{output}</div>
       </header>
     </div>
   );
